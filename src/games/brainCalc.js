b7b3baddd6f79962932440cdeb2873602f80f8aa
Vitalii.Marsh <kaarido@yandex.ru>
2024-05-15 22:03:57 +0300

    rename brainCalc.js

---
+++ src/games/brainCalc.js
@@ -0,0 +1,34 @@
+import playBrainGames from '../index.js';
+import randomNum from '../randomNumbers.js';
+
+const calc = (randomNum1, randomNum2, operator) => {
+    switch(operator) {
+        case '+': 
+            return randomNum1 + randomNum2;
+        case '-': 
+            return randomNum1 - randomNum2;
+        case '*': 
+            return randomNum1 * randomNum2;
+    }
+};
+
+const randomOperators = () => {
+    const operators = ['+', '-', '*'];
+    const numOperator = Math.floor(Math.random() * 3);
+    return operators[numOperator];
+};
+
+const preface = 'What is the result of the expression?';
+
+const questionAswer = () => {
+const randomNum1 = randomNum();
+const randomNum2 = randomNum();
+const operator = randomOperators();
+const question = `${randomNum1} ${operator} ${randomNum2}`;
+const answer = String(calc(randomNum1, randomNum2, operator));
+return [question, answer];
+};
+
+const play = () => playBrainGames(questionAswer, preface);
+
+export default play;
